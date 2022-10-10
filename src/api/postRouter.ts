af78b09a1139ede13f0964a830eb74a5c620e9d1
Claudemir-Pontes <cs.systemsdev@gmail.com>
2022-10-10 14:07:58 -0400

    feat: added the CRUD functions in the Post table

--- src/api/postRouter.ts
+++ src/api/postRouter.ts
@@ -1,19 +1,50 @@
-import { Router } from "express"
+import { response, Router } from "express"
+import { request } from "http"
 import { PostService } from "../services/postService"
 
 
 const router = Router()
-const profileService = new PostService()
+const postService = new PostService()
 
 router.post('/', async (req, res) => {
     try {
         const post = req.body
 
-        await profileService.createPost(post)
+        await postService.createPost(post)
 
         res.status(201).send("Post created successfully.")
     } catch (err) {
         res.status(400).send(err.message)
+    }
+})
+
+router.get('/', async (req, res) => {
+    try {
+        const post = await postService.getPost()
+        res.status(200).send(post)
+    } catch (err) {
+        res.status(400).send(err.message)
+    }
+})
+
+router.put("/", async (request, response) => {
+    try {
+        const postTDO = request.body
+        await postService.updatePost(postTDO)
+        response.status(200).send("Post successfully updated.")
+    } catch (error) {
+        response.status(400).send(error.message)
+    }
+    
+})
+
+router.delete("/", async (request, response) => {
+    try {
+        const dataTDO = request.body
+        await postService.deletePost(dataTDO)
+        response.status(200).send("Post successfully deleted.")
+    } catch (error) {
+        response.status(400).send(error.message)
     }
 })
 
--- src/services/postService.ts
+++ src/services/postService.ts
@@ -2,9 +2,73 @@ import { prisma } from "../models/prismaModel"
 
 export class PostService {
 
-    async createPost(post){
+    async createPost(post) {
         await prisma.post.create({
-            data: post
+            data: {
+                title: post.title,
+                body: post.body,
+                author: {
+                    connect: {
+                        id: post.author
+                    }
+                }
+            }
+
+        })
+    }
+
+    async getPost() {
+        const post = await prisma.post.findMany()
+        return post
+    }
+
+    async updatePost(postTDO) {
+
+        const userRequest = postTDO.userRequest
+
+        // checks if the user owns the Post
+        const userPost = await prisma.post.findFirst({
+            where: {
+                id: postTDO.id
+            }
+        })
+
+        if (userRequest != userPost.authorId) {
+            throw new Error("Access denied.")
+        }
+
+        // updates the Post.
+        await prisma.post.update({
+            where: {
+                id: postTDO.id
+            },
+            data: {
+                title: postTDO.title,
+                body: postTDO.body,
+                published: postTDO.published
+            }
+        })
+    }
+
+    async deletePost(dataTDO) {
+        const userRequest = dataTDO.userRequest
+
+        // checks if the user owns the Post
+        const userPost = await prisma.post.findFirst({
+            where: {
+                id: dataTDO.id
+            }
+        })
+
+        if (userRequest != userPost.authorId) {
+            throw new Error("Access denied.")
+        }
+
+        // delete the post
+        await prisma.post.delete({
+            where: {
+                id: dataTDO.id
+            }
         })
     }
 }
