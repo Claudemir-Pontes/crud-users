37295dcde98622c59fe46e6a4ee525a6fc8ec572
Claudemir Pontes <cs.systemsdev@gmail.com>
2023-02-06 14:47:28 -0400

    test: github

--- src/api/index.ts
+++ src/api/index.ts
@@ -2,11 +2,17 @@ import { Router } from "express"
 import usersRouter from "./usersRouter"
 import profilesRouter from "./profileRouter"
 import postsRouter from "./postRouter"
+import { json } from "stream/consumers"
 
 const router = Router()
 
 router.get('/', (request, response) => {
-    return response.status(200).send('<h1>CRUD</h1>')
+    const res =[{
+        'crud':'crudddddddddddddddddddddddddddd',
+        'cru':'cruuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuu'
+    }]
+    
+    return response.status(200).json(res)
 })
 
 router.use('/users', usersRouter)
--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
 //express application configuration file
 import express from 'express'
 import  routers  from './api'
+import cors from 'cors'
 
 const app = express()
 
+app.use(cors())
 app.use(express.json())
 app.use('/',routers)
 
